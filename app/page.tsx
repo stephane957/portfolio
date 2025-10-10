1b35bc4ce159dadbb31d08faa854f362c3728448
Stéphane Toyo Demanou <stephanetoyo14@gmail.com>
2025-10-09 21:44:49 -0400

    Project Setup

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <main className="relative text-black flex justify-content 
+    items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <h1>Welcome to My Portfolio</h1>
+      </div>
+    </main>
+  )
+}
+
+export default HomePage
